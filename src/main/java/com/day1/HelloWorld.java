d44aacc84ccc54bb72edd78a1046e5a1c43f626a
YS_workPC <ong_ys86@yahoo.com.sg>
2021-12-06 22:27:25 +0800

    HelloWorld from home

---
+++ src/main/java/com/day1/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.day1;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
